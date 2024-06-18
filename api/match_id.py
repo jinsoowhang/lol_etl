cf7d6c2a539d68afcc03e0d877825e4650520a0d
jwtreones <jwtreones@gmail.com>
2024-06-17 21:24:49 -0700

    bringing top 20 games for more players

--- api/match_id.py
+++ api/match_id.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import sys
+import time
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -10,14 +11,31 @@ from config.config import GET_MATCH_IDS_BY_PUUID_URL, API_KEY
 class MatchID:
     def __init__(self, puuid):
         self.puuid = puuid 
+        self.match_ids = set()  # Use a set to avoid duplicate match IDs
 
-    def get_match_ids(self):
-        full_url = f"{GET_MATCH_IDS_BY_PUUID_URL}/{self.puuid}/ids?api_key={API_KEY}"
-        response = requests.get(full_url)
+    def get_match_ids(self, start=0, count=20):
+        # Adjusted to handle pagination and rate limits
+        params = {
+            'start': start,
+            'count': count,
+            'api_key': API_KEY
+        }
+        full_url = f"{GET_MATCH_IDS_BY_PUUID_URL}/{self.puuid}/ids"
+        response = requests.get(full_url, params=params)
+
         if response.status_code == 200:
-            # remove [:2] once we want to get all the matches
             match_ids = response.json()
-            return match_ids[:2]
+            self.match_ids.update(match_ids)  # Add new match IDs to the set
+            return match_ids
         else:
             print(f"Error: {response.status_code}")
-            return None
+            return []
+        
+    def fetch_first_20_match_ids(self):
+        # Fetch only the first 100 match IDs
+        start = 0
+        count = 20
+        
+        self.get_match_ids(start=start, count=count)
+        return list(self.match_ids)[:20]  # Return up to 20 match IDs
+
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # main.py
 
 from etl.etl_process import ETLProcess
-from config.config import TAG_LINE, GAME_NAME_TANKTOP
+from config.config import TAG_LINE, GAME_NAME_TANKTOP, GAME_NAME_VELBRI
+
+# List of game names
+game_names = [GAME_NAME_TANKTOP, GAME_NAME_VELBRI]
 
 # Run the ETL process
-etl = ETLProcess(GAME_NAME_TANKTOP, TAG_LINE)
+etl = ETLProcess(game_names, TAG_LINE)
 etl.run()
 
