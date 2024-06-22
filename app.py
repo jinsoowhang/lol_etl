24aa868dabaae8c4d7679d7d9ff0617b6e7331ab
Jinsoo Whang <74080028+jinsoowhang@users.noreply.github.com>
2024-06-22 18:39:33 +0000

    adding first visualization

--- app.py
+++ app.py
@@ -27,10 +27,28 @@ st.divider()
 test_df = df.copy()
 test_df = test_df.sort_values(by=['summoner_name', 'game_date'])
 test_df['row_id'] = test_df.groupby('summoner_name').cumcount() + 1 
+test_df['row_id'] = test_df['row_id'].astype(str)
 test_df['cumsum_wins'] = df.sort_values(by=['game_date']).groupby('summoner_name')['win'].cumsum()
+tanktopmastr_df = test_df[test_df['summoner_name'] == 'TanktopMastr']
+velbri_df = test_df[test_df['summoner_name'] == 'Velbri']
+camachbro_df = test_df[test_df['summoner_name'] == 'Camachbro']
 
 st.markdown('## Wins over the past 20 games')
 
-# test_df[test_df['summoner_name'] == 'TanktopMastr'].plot(kind='line', grid=True, color='blue')
+fig, ax = plt.subplots()
+ax.plot(tanktopmastr_df['row_id'], tanktopmastr_df['cumsum_wins'], marker='o', label='TanktopMastr')
+ax.plot(velbri_df['row_id'], velbri_df['cumsum_wins'], marker='x', label='Velbri')
+ax.plot(camachbro_df['row_id'], camachbro_df['cumsum_wins'], marker='s', label='Camachbro')
 
-# plt.show()
+# Set plot labels and title
+ax.set_xlabel('Game Number')
+ax.set_ylabel('Cumulative Wins')
+ax.set_title('Cumulative Wins Over Time')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Add legend
+ax.legend()
+
+# Render the plot with Streamlit
+st.pyplot(fig)
