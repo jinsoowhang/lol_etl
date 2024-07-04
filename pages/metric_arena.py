a59ae0932b96934114d919f702ad15013f444c6e
Jinsoo Whang <74080028+jinsoowhang@users.noreply.github.com>
2024-07-04 21:23:05 +0000

    updated metric arena page design

--- pages/metric_arena.py
+++ pages/metric_arena.py
@@ -23,31 +23,41 @@ df = pd.read_parquet(parquet_file_path)
 # Image Folder Path
 image_folder_path = 'assets/images'
 
-##########################################
-########### Show full dataset ############
-##########################################
-
-with st.expander("See full data table"):
-    st.write(df)
-
-################################
-####### Choose a metric ########
-################################
+########################
+####### Filters ########
+########################
 
 non_metric_cols = ['summoner_name', 'player_index', 'match_id', 'game_mode', 'game_date', 'ability_uses', 'champion_name']
 list_of_metric_cols = [col for col in df.columns if col not in non_metric_cols]
 
-st.write("## Choose a metric")
-chosen_metric = st.selectbox("Metrics", options=list_of_metric_cols, index=1)
+# Change Column Name to be clean to users
+list_of_metrics_clean_col_name = [col.replace('_', ' ').title() for col in list_of_metric_cols]
+
+with st.sidebar:
+
+    st.markdown("# 🛠️ Filters")
+    chosen_metric = st.radio("Metrics", options=list_of_metrics_clean_col_name, index=1)
+
+# Change Column Name back to data compatible
+chosen_metric = chosen_metric.replace(' ', '_').lower()
+
+########################
+####### Metrics ########
+########################
 
 # Rank for each Summoner
 rank_by_metric = df.groupby(['summoner_name'], as_index=False)[chosen_metric].mean().sort_values(by=chosen_metric, ascending=False)
+
+# Title
+column_name = rank_by_metric.columns[1] 
+column_name = column_name.replace('_', ' ').lower()
+
+st.markdown(f"# Who is the BEST in {column_name}?")
 
 # Rank
 col1, col2, col3 = st.columns(3)
 
 # Column Name
-column_name = rank_by_metric.columns[1] 
 
 with col1:
     for summoner in df['summoner_name'].unique():
