b141903fc27c04f9a1ce27c4ddc932039dc689f8
jwtreones <jwtreones@gmail.com>
2024-07-08 20:01:45 -0700

    adding Kingvei data

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # main.py
 
 from etl.etl_process import ETLProcess
-from config.config import TAG_LINE, GAME_NAME_TANKTOP, GAME_NAME_VELBRI, GAME_NAME_CAMACHBRO
+from config.config import TAG_LINE, GAME_NAME_TANKTOP, GAME_NAME_VELBRI, GAME_NAME_CAMACHBRO, GAME_NAME_KINGVEI
 
 # List of game names
-game_names = [GAME_NAME_TANKTOP, GAME_NAME_VELBRI, GAME_NAME_CAMACHBRO]
+game_names = [GAME_NAME_TANKTOP, GAME_NAME_VELBRI, GAME_NAME_CAMACHBRO, GAME_NAME_KINGVEI]
 
 # Run the ETL process
 etl = ETLProcess(game_names, TAG_LINE)
